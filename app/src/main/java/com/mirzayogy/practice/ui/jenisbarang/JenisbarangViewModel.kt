f4790989016c263c9f76226514025fab811bde61
mirzayogy <mirza.yogy@gmail.com>
2021-07-17 13:29:34 +0800

    Part 5

--- app/src/main/java/com/mirzayogy/practice/ui/jenisbarang/JenisbarangViewModel.kt
+++ app/src/main/java/com/mirzayogy/practice/ui/jenisbarang/JenisbarangViewModel.kt
@@ -4,11 +4,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mirzayogy.practice.model.Jenisbarang
+import com.mirzayogy.practice.model.JenisbarangData
+import com.mirzayogy.practice.model.JenisbarangResponse
 import com.mirzayogy.practice.network.Api
 import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class JenisbarangViewModel : ViewModel() {
     val response = MutableLiveData<Jenisbarang>()
+    val createResponse = MutableLiveData<Response<JenisbarangResponse>>()
 
     fun getJenisbarang() {
         viewModelScope.launch {
@@ -18,6 +22,13 @@ class JenisbarangViewModel : ViewModel() {
             } catch (e: Exception) {
                 response.value = null
             }
+        }
+    }
+
+    fun create(jenisbarangData: JenisbarangData) {
+        viewModelScope.launch {
+            val response = Api.retrofitService.create(jenisbarangData)
+            createResponse.value = response
         }
     }
 }
